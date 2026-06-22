6d44ca2fe2ac8f6b197307b0fde06988821e1206
matelat-daw <cesarmatelat@gmail.com>
2026-06-22 19:12:49 +0100

    Update Domitila HOME, Fine

--- src/main/java/domitila/auth/proyecto/Proyecto.java
+++ src/main/java/domitila/auth/proyecto/Proyecto.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
+@Table(name = "proyecto")
 public class Proyecto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "name")
     private String name;
 }
